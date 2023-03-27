19ab497d46682b5e9dc6b3e2b67781042a47c30b
ShahnawazKhan190 <129104117+ShahnawazKhan190@users.noreply.github.com>
2023-03-27 23:24:13 +0530

    Add files via upload

---
+++ first.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class first{
+	public static void main(String[] args){
+		System.out.println("AA GAYAAAAA!!!");
+		Scanner inp = new Scanner(System.in);
+		int num = inp.nextInt();
+		System.out.println("AA GAYAAAAA!!! ye dekho -> "+num);
+	}
+}
