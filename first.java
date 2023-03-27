bda4a6a511a5db79c2a3ef4048d92fde2baa39a9
ShahnawazKhan190 <129104117+ShahnawazKhan190@users.noreply.github.com>
2023-03-28 01:07:18 +0530

    Delete first.java

--- first.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-class first{
-	public static void main(String[] args){
-		System.out.println("AA GAYAAAAA!!!");
-		Scanner inp = new Scanner(System.in);
-		int num = inp.nextInt();
-		System.out.println("AA GAYAAAAA!!! ye dekho -> "+num);
-	}
-}
